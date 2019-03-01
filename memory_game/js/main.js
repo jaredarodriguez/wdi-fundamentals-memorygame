f13c0e92265b6e4f7636f864626c2f981b6392da
Jared Rodriguez <jaredarodriguez@gmail.com>
2019-03-01 15:15:42 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,28 @@
-console.log ("up and running!");
 var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cardTwo = "king";
+var cardsInPlay = [];
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+
+var cards = ["queen", "queen", "king", "king"];
+
 console.log ("User flipped " + cardOne);
-console.log ("User flipped " + cardThree);
+console.log ("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2);
+else if (cardsInPlay[0] === cardsInPlay[1]){
+	alert("You found a match!");
+}
+if (cardsInPlay[0] !== cardsInPlay[1]){
+	alert("Sorry try again!");
+}
+
+
+
+
+
+
+
+
+
